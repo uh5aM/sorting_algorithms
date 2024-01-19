9c674b666a327b890e43864dfdbaa97b5fcdf23b
uh5aM <ahmadabubakarmusa0@gmail.com>
2024-01-19 06:48:08 +0000

    cock

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * cocktail_sort_list - A function that sorts doubly linked list integers
+ * in ascending order
+ *
+ * @list: the list passed to the function
+ * Return: no return value
+ */
+
+void cocktail_sort_list(listint_t **list)
+{
+}
